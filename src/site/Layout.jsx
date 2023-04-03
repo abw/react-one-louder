b1eb8dd8646ba49a8b094a94e9a9e545976f3367
Andy Wardley <abw@wardley.org>
2023-04-03 10:37:47 +0100

    ScrollToTop on page transition

--- src/site/Layout.jsx
+++ src/site/Layout.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Sidebar from './Sidebar.jsx'
+import ScrollToTop from './ScrollToTop.jsx'
 
 const Layout = () =>
   <div id="layout">
+    <ScrollToTop/>
     <aside>
       <Sidebar/>
     </aside>
